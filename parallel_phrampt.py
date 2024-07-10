f385756eb02ca075034f9678c8d4e95aa627fade
Patrick Cross <pcross@kestrel.chem.wisc.edu>
2024-07-10 18:06:37 -0500

    added more functionality to parallel methods

--- parallel_phrampt.py
+++ parallel_phrampt.py
@@ -1,22 +1,41 @@
 import multiprocessing as mp
-import os    
+import os
+from lammps import lammps
 
 #----------------------------------------------------------------------------------------------------------------#
 
 # method that does the the calculating in parallel
-def parallel_Calc(self):
+def parallel_Calc(in_file, proc_num):
+
+    lmp = lammps()
+    lmp.file(in_file)
+    natoms = lmp.get_natoms()
+
+#----------------------------------------------------------------------------------------------------------------#
+
+# method for creating parallel pools
+
+def make_parallel(in_file, natoms):
 
     # get number of available processors
     procs = len(os.sched_getaffinity(0)) - 1
-    if procs > self._natoms:
+
+    # if using more processors than atoms, change processor count to be number of atoms
+    if procs > natoms:
         import warnings
         warnings.warn('''There is no benefit to using more processors than number of atoms.\n
                         Number of parallel jobs will be set to number of atoms.
                         ''')
         
-        procs = self._natoms
-
-    
+        procs = natoms
 
     # create pool
     pool = mp.Pool(processes=procs)
+
+    # setup arguments
+    arg1 = [in_file for i in range(procs)]
+    arg2 = [*range(procs)]
+    args = [*zip(arg1, arg2)]
+
+    # submit arguments to pool jobs
+    calc_output = pool.starmap(parallel_Calc, iterable = args)
